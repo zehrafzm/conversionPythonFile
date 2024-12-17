a67ab25dc3721a90bdefacd2b13f133c502671aa
zehrafzm <105994419+zehrafzm@users.noreply.github.com>
2024-12-17 19:01:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def upload_video():
     return send_file(output_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=port)
